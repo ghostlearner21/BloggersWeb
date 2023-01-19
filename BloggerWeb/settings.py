5c556db3abc4fbce1e4767600895fae21c5706ee
ghostlearner21 <103304731+ghostlearner21@users.noreply.github.com>
2023-01-19 11:54:39 +0530

    Update settings.py
    security updates

--- BloggerWeb/settings.py
+++ BloggerWeb/settings.py
@@ -17,10 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dew4uoypkjsat$s%08%k!y_h39u7ki*@ebcn5g6kxub8aun@kx'
+# See https://docs.djangoproject.com/en/4.1/howto/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
